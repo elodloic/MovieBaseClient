40630000310e8a2381a57e5360d7a135cd96e284
elodloic <95284344+elodloic@users.noreply.github.com>
2024-09-18 16:25:47 +0200

    Update movie-card.jsx
    Corrected typo

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,7 +7,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Img variant="top" src={movie.image} alt={movie.title}/>
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.author}</Card.Text>
+        <Card.Text>{movie.director}</Card.Text>
         <Button onClick={() => onMovieClick(movie)} variant="secondary">
           Open
         </Button>
