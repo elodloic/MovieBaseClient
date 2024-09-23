413647e565cfdf387baf255162a3e02b93cb9e38
elodloic <95284344+elodloic@users.noreply.github.com>
2024-09-23 12:35:48 +0200

    Filter movies
    A search bar has been added to the main view.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./main-view.scss";
 import { ProfileView } from "../profile-view/profile-view";
@@ -17,10 +18,7 @@ export const MainView = () => {
   const [user, setUser] = useState(storedUser ? storedUser : null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
-
-  const favoriteMovies = movies.filter((movie) =>
-    user && user.FavoriteMovies.includes(movie.id)
-    );
+  const [searchMovie, setSearchMovie] = useState("");
 
   useEffect(() => {
     if (!token) {
@@ -41,7 +39,6 @@ export const MainView = () => {
             genre: movie.Genre.Name,
           };
         });
-
         setMovies(moviesFromApi);
       })
       .catch((error) => {
@@ -49,103 +46,125 @@ export const MainView = () => {
       });
   }, [token]);
 
-   {
-    return (
-      <BrowserRouter>
-        <NavigationBar
-          user={user}
-          onLoggedOut={() => {
-            setUser(null);
-          }}
-        />
-        <Row className="justify-content-md-center">
-          <Routes>
-            <Route
-              path="/signup"
-              element={
-                <>
-                  {user ? (
-                    <Navigate to="/" />
-                  ) : (
-                    <Col md={5}>
-                      <SignupView />
-                    </Col>
-                  )}
-                </>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <>
-                  {user ? (
-                    <Navigate to="/" />
-                  ) : (
-                    <Col md={5}>
-                      <LoginView onLoggedIn={(user, token) => {
+  const filteredMovies = movies.filter((movie) => {
+    const regex = new RegExp(`\\b${searchMovie}`, "i"); // RegExp to only match at the beginning of words
+    return regex.test(movie.title);
+  });
+
+  return (
+    <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+      />
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignupView />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView
+                      onLoggedIn={(user, token) => {
                         setUser(user);
                         setToken(token);
-                      }} />
+                      }}
+                    />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/movies/:movieId"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <Col md={8}>
+                    <MovieView movies={movies} setUser={setUser} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {/* Filter input */}
+                    <Col md={12} className="mb-4">
+                      <Form>
+                        <Form.Control
+                          type="text"
+                          placeholder="Search movies"
+                          value={searchMovie}
+                          onChange={(e) => setSearchMovie(e.target.value)}
+                        />
+                      </Form>
                     </Col>
-                  )}
-                </>
-              }
-            />
-            <Route
-              path="/movies/:movieId"
-              element={
-                <>
-                  {!user ? (
-                    <Navigate to="/login" replace />
-                  ) : movies.length === 0 ? (
-                    <Col>The list is empty!</Col>
-                  ) : (
-                    <Col md={8}>
-                      <MovieView movies={movies} setUser={setUser} />
-                    </Col>
-                  )}
-                </>
-              }
-            />
-            <Route
-              path="/"
-              element={
-                <>
-                  {!user ? (
-                    <Navigate to="/login" replace />
-                  ) : movies.length === 0 ? (
-                    <Col>The list is empty!</Col>
-                  ) : (
-                    <>
-                      {movies.map((movie) => (
+
+                    {/* Filter results */}
+                    {filteredMovies.length === 0 ? (
+                      <p>No matching movies.</p>
+                    ) : (
+                      filteredMovies.map((movie) => (
                         <Col className="mb-4" key={movie.id} md={3}>
                           <MovieCard movie={movie} />
                         </Col>
-                      ))}
-                    </>
-                  )}
-                </>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <>
-                  {!user ? (
-                    <Navigate to="/login" replace />
-                  ) : movies.length === 0 ? (
-                    <Col>The list is empty!</Col>
-                  ) : (
-                    <Col md={8}>
-                      <ProfileView user={user} movies={movies} setUser={setUser} />
-                    </Col>
-                  )}
-                </>
-              }
-            />
-          </Routes>
-        </Row>
-      </BrowserRouter>
-    );
-  }
+                      ))
+                    )}
+                  </>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <Col md={8}>
+                    <ProfileView user={user} movies={movies} setUser={setUser} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
+  );
 };
